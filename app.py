ce9c27692490baa0864a4888c95506221c01d895
Dokeun Oh <dokeun.oh@car-ewallet.de>
2020-08-23 03:26:28 +0200

    split mock file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+from mock import *
 
 app = Flask(__name__)
     
@@ -24,63 +25,3 @@ def getGraphInfo():
 
 	return jsonify(d)
 
-# MOCK
-def create_1day():
-	return [
-		{
-		    "name": "ms",
-		    "ticker": "MSFT",
-		    "value": 5.3,
-		    "unit": "%"
-		},{
-		    "name": "apple",
-		    "ticker": "AAPL",
-		    "value": -10,
-		    "unit": "%"
-		}
-	]
-
-def create_1week():
-	return [
-		{
-		    "name": "ms",
-		    "ticker": "MSFT",
-		    "value": 10.3,
-		    "unit": "%"
-		},{
-		    "name": "apple",
-		    "ticker": "AAPL",
-		    "value": -20,
-		    "unit": "%"
-		}
-	]
-
-def create_per():
-	return [
-		{
-		    "name": "ms",
-		    "ticker": "MSFT",
-		    "value": 5.3,
-		    "unit": ""
-		},{
-		    "name": "apple",
-		    "ticker": "AAPL",
-		    "value": 33,
-		    "unit": ""
-		}
-	]
-
-def create_pbr():
-	return [
-		{
-		    "name": "ms",
-		    "ticker": "MSFT",
-		    "value": 44.3,
-		    "unit": ""
-		},{
-		    "name": "apple",
-		    "ticker": "AAPL",
-		    "value": 12.4,
-		    "unit": ""
-		}
-	]
---
+++ mock.py
@@ -0,0 +1,60 @@
+# MOCK
+def create_1day():
+	return [
+		{
+		    "name": "ms",
+		    "ticker": "MSFT",
+		    "value": 5.3,
+		    "unit": "%"
+		},{
+		    "name": "apple",
+		    "ticker": "AAPL",
+		    "value": -10,
+		    "unit": "%"
+		}
+	]
+
+def create_1week():
+	return [
+		{
+		    "name": "ms",
+		    "ticker": "MSFT",
+		    "value": 10.3,
+		    "unit": "%"
+		},{
+		    "name": "apple",
+		    "ticker": "AAPL",
+		    "value": -20,
+		    "unit": "%"
+		}
+	]
+
+def create_per():
+	return [
+		{
+		    "name": "ms",
+		    "ticker": "MSFT",
+		    "value": 5.3,
+		    "unit": ""
+		},{
+		    "name": "apple",
+		    "ticker": "AAPL",
+		    "value": 33,
+		    "unit": ""
+		}
+	]
+
+def create_pbr():
+	return [
+		{
+		    "name": "ms",
+		    "ticker": "MSFT",
+		    "value": 44.3,
+		    "unit": ""
+		},{
+		    "name": "apple",
+		    "ticker": "AAPL",
+		    "value": 12.4,
+		    "unit": ""
+		}
+	]
